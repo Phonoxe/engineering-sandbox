b00e0dff598ef2ff9d51b12887a29f612ef578f6
Leopold Stiffel <leopold.stiffel@epfedu.fr>
2026-02-05 13:02:01 +0100

    The code is able to extract the data stored in data.json and print it regarding the expiration date

---
+++ src/exctractRegistered.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from datetime import datetime
+from datetime import date
+import json
+
+# chemin vers la racine du projet
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# chemin vers le fichier json
+DATA_FILE = BASE_DIR / "data" / "data.json"
+
+with open(DATA_FILE, "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+
+listExpired = []
+listNotExpired = []
+
+for item in data:
+    expiration_date = datetime.strptime(item["date"], "%d/%m/%Y")
+    expired = expiration_date < datetime.now()
+    if expired:
+        listExpired.append(item)
+    else:
+        listNotExpired.append(item)
+        distanceFromExpiration = (expiration_date.date() - date.today()).days
+        item.update({"days_until_expiration": distanceFromExpiration})
+
+listNotExpired.sort(key=lambda x: x["days_until_expiration"])
+
+print("Expired items:")
+for expired_item in listExpired:
+    print("Name:", expired_item["name"], "| Expiration Date:", expired_item["date"])
+print("\nNot expired items:")
+for not_expired_item in listNotExpired:
+    print(
+        "Name:",
+        not_expired_item["name"],
+        "| Expiration Date:",
+        not_expired_item["date"],
+        "| Days until expiration:",
+        not_expired_item["days_until_expiration"],
+    )
