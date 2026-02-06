8a39d8dc08d6bcbb392a21582ffdcaa9f87fbef4
Challehnge <jade.zhang@epfedu.fr>
2026-02-06 23:51:08 +0100

    Fonctional code with added comments

--- src/Register_Product.py
+++ src/Register_Product.py
@@ -2,10 +2,12 @@ import json
 from pathlib import Path
 from datetime import datetime
 
+# Define the path to the data file
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = BASE_DIR / "data"
 DATA_FILE = DATA_DIR / "data.json"
 
+# Ensure the data directory exists
 if DATA_FILE.exists():
     with open(DATA_FILE, "r", encoding="utf-8") as f:
         products = json.load(f)
@@ -14,6 +16,7 @@ else:
 
 product_name = input("Enter product name : ")
 
+# Validate the expiry date format
 while True:
     input_expiry_date = input("Enter Expiry Date (DD/MM/YYYY) : ")
     try:
@@ -26,5 +29,6 @@ new_product = {"Name": product_name, "Date": product_expiry_date.strftime("%d/%m
 
 products.append(new_product)
 
+# Save the updated products list back to the JSON file
 with open(DATA_FILE, "w", encoding="utf-8") as file:
     json.dump(products, file)
